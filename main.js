56c56d821ff0253699cbd54052b1beaabe4c4176
n1ckdevops <nickdevops6@gmail.com>
2024-04-15 12:44:48 +0300

    fix

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const todoControll = document.querySelector(".todo-control");
 const headerInput = document.querySelector(".header-input");
 const todoList = document.querySelector(".todo-list");
 const todoCompleted = document.querySelector(".todo-completed");
+// const todoRemove = document.querySelector(".todo-remove");
 // отрисовывать будет перебором определенный массив
 // создаем массов в виде обьектов в котором будет хранить каждую
 // тудушку
@@ -22,25 +23,31 @@ const render = function () {
       '<button class="todo-remove">"</button>' +
       '<button class="todo-complete"></button>' +
       "</div>;";
-    if (item.completed && headerInput.value !== "") {
+    if (item.completed) {
       todoCompleted.append(li);
     } else {
       todoList.append(li);
     }
-    console.log(headerInput.value);
     li.querySelector(".todo-complete").addEventListener("click", function () {
       item.completed = !item.completed;
+      render();
+    });
+    li.querySelector(".todo-remove").addEventListener("click", function () {
+      const index = toDoData.indexOf(item);
+      toDoData.splice(index, 1);
       render();
     });
   });
 };
 todoControll.addEventListener("submit", function (event) {
   event.preventDefault();
-  const newToDo = {
-    text: headerInput.value,
-    completed: false,
-  };
-  toDoData.push(newToDo);
-  headerInput.value = "";
-  render();
+  if (headerInput.value !== "") {
+    const newToDo = {
+      text: headerInput.value,
+      completed: false,
+    };
+    toDoData.push(newToDo);
+    headerInput.value = "";
+    render();
+  }
 });
